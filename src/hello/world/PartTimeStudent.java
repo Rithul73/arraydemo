e6e8abc3bc23df8acb3bfc1ba8d85539963c9944
Rithul Chandran <chanrith@sheridancollege.ca>
2024-01-24 14:37:55 -0500

    added class

---
+++ src/hello/world/PartTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author User
+ */
+public class PartTimeStudent extends HelloWorld {
+    
+    private int numOfCourses;
+
+    public PartTimeStudent() {
+    }
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+}
